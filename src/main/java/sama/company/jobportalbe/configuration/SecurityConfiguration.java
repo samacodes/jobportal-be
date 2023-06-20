b6f7bfdce607de9fdb7cba70688e3cc869762c68
Sama Acharya <asandhya648@gmail.com>
2023-06-20 22:47:15 +0545

    add basic security configuration

---
+++ src/main/java/sama/company/jobportalbe/configuration/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package sama.company.jobportalbe.configuration;
+
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+}
